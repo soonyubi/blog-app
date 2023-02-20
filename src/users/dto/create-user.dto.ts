cd1e80ffdfed8160bacaecef2ea9f31e19ab000d
soonyubi <soonyubi1@naver.com>
2023-02-20 14:06:36 +0900

    FEAT: Create dto in auth and user module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    email:string;
+    password:string;
+    firstname:string;
+    lastname:string;
+    profile_img_url:string;
+    refreshToken : string;
+}
