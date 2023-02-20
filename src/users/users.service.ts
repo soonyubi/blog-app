a2be388f0977f48ef965d7766e7dad176bba4b17
soonyubi <soonyubi1@naver.com>
2023-02-20 14:31:34 +0900

    FEAT: Update User service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,6 +23,10 @@ export class UsersService {
     return this.userRepository.findOne({where:{id}});
   }
 
+  findByUserEmail(email: string){
+    return this.userRepository.findOne({where:{email}});
+  }
+
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.userRepository.update(id, updateUserDto);
   }
