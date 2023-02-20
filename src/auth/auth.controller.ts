dc2c5a3b8e5a3f582c2a7ff4a81839024efa05d5
soonyubi <soonyubi1@naver.com>
2023-02-20 14:30:42 +0900

    FEAT: Update Auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+   
+  
+  @Post('signup')
+  signup(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+
+  @Post('signin')
+  signin(@Body() authDto: AuthDto) {
+    return this.authService.signIn(authDto);
+  }
+
+  @Get('logout')
+  logout(@Req() req : Request) {
+    return this.authService.logout(req.user['id']);
+  }
 }
